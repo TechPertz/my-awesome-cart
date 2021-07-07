d04d3bcb66c8a4f8404e5045995a059fdf43d11e
TechPertz <reetnandy@gmail.com>
2021-07-08 00:50:50 +0530

    added basic models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.base import Model
+# from django.db.models.fields import DateTime
 
 # Create your models here.
+
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=300)
+    pub_date = models.DateField()
