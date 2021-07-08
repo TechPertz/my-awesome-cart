6a7645b5c01deae7035da04b997e67f3878c060a
TechPertz <reetnandy@gmail.com>
2021-07-08 23:25:26 +0530

    before adding loops in index.html for slider

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+
+from math import ceil
 
 # Create your views here.
 
 def index (request):
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nslides = n//4 + ceil((n/4) - (n//4))
+    params ={'no_of_slides': nslides, 'range': range(1, nslides),'product': products}
     return render(request,'shop/index.html')
 
 def about (request):
