32d0960d2b630b76287ee19f72d46b3a6a17c34b
TechPertz <reetnandy@gmail.com>
2021-07-16 23:46:54 +0530

    added cart using js and jquery

--- shop/views.py
+++ shop/views.py
@@ -6,28 +6,44 @@ from math import ceil
 
 # Create your views here.
 
-def index (request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nslides = n//4 + ceil((n/4) - (n//4))
-    params ={'no_of_slides': nslides, 'range': range(1, nslides),'product': products}
-    return render(request,'shop/index.html', params)
 
-def about (request):
-    return render(request,'shop/about.html')
+def index(request):
+    # products = Product.objects.all()
+    # print(products)
+    # params ={'no_of_slides': nslides, 'range': range(1, nslides),'product': products}
+    # allprods = [[products, range(1, nslides), nslides], [
+    #     products, range(1, nslides), nslides]]
+    allprods = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(prod)
+        nslides = n//4 + ceil((n/4) - (n//4))
+        allprods.append([prod, range(1, nslides), nslides])
+    params = {'allprods': allprods}
+    return render(request, 'shop/index.html', params)
 
-def contact (request):
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+
+def contact(request):
     return HttpResponse("We are at Contact")
 
-def tracker (request):
+
+def tracker(request):
     return HttpResponse("We are at tracker")
 
-def search (request):
+
+def search(request):
     return HttpResponse("We are at Search")
 
-def productview (request):
+
+def productview(request):
     return HttpResponse("We are at Product View")
 
-def checkout (request):
+
+def checkout(request):
     return HttpResponse("We are at Checkout")
