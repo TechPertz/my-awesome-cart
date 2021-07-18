61bc88d161993a9a29705275ea3c5ad034224d87
TechPertz <reetnandy@gmail.com>
2021-07-18 23:19:02 +0530

    updates with contact db

--- shop/urls.py
+++ shop/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="ShopHome"),
-    path('about', views.about, name="AboutUs"),
-    path('contact', views.contact, name="ContactUs"),
-    path('tracker', views.tracker, name="TrackingStatus"),
-    path('search', views.search, name="Search"),
+    path('about/', views.about, name="AboutUs"),
+    path('contact/', views.contact, name="ContactUs"),
+    path('tracker/', views.tracker, name="TrackingStatus"),
+    path('search/', views.search, name="Search"),
     path('products/<int:myid>', views.products, name="Products"),
-    path('checkout', views.checkout, name="Checkout"),
+    path('checkout/', views.checkout, name="Checkout"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 
 from math import ceil
 
@@ -25,7 +25,12 @@ def about(request):
 
 def contact(request):
     if request.method == "POST":
-        print(request)
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
     return render(request, 'shop/contact.html')
 
 def tracker(request):
