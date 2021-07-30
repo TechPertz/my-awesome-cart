e10ad4b479aae5347a8d3df0db3c4760047a7359
TechPertz <reetnandy@gmail.com>
2021-07-30 23:48:18 +0530

    tracker page

--- shop/views.py
+++ shop/views.py
@@ -35,8 +35,8 @@ def contact(request):
 
 def tracker(request):
     if request.method == "POST":
-        orderId = request.get('orderId', '')
-        email = request.get('email', '')
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
         try:
             order = Orders.objects.filter(order_id = orderId, email = email)
             if len(order)>0:
@@ -44,12 +44,12 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text' : item.update_desc, 'time' : item.timestamp})
-                    response = json.dumps(updates)
-                    return HttpResponse(response)
-            else:
-                pass
+                    response = json.dumps([updates,order[0].items_json], default=str)
+                return HttpResponse(response)
+            else: 
+                return HttpResponse('{}')
         except Exception as e:
-            pass
+            return HttpResponse('{}')
 
     return render(request, 'shop/tracker.html')
 
