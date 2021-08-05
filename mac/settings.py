e580e255305de8ae1e37806edee798b7db7d6697
TechPertz <reetnandy@gmail.com>
2021-08-06 00:08:54 +0530

    gitignore

--- mac/settings.py
+++ mac/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from stuff import MY_SECRET_KEY
+from . import stuff
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r4kla8rh$nrg-*+zo&@0o*+8$8$493_*$a+pe$1c4kegu(*3=s'
+SECRET_KEY = MY_SECRET_KEY
+# SECRET_KEY = 'django-insecure-r4kla8rh$nrg-*+zo&@0o*+8$8$493_*$a+pe$1c4kegu(*3=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
