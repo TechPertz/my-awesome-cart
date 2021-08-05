fd78c6bb5144d8bfb702061888db0311f5e68643
TechPertz <reetnandy@gmail.com>
2021-08-06 00:16:20 +0530

    final gitignore

--- mac/settings.py
+++ mac/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from stuff import MY_SECRET_KEY
-from . import stuff
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
