85fe5e47816ecdf5f3a47a1117ef125d28f4f3da
TechPertz <reetnandy@gmail.com>
2021-08-11 23:54:12 +0530

    implemented basic search

--- shop/views.py
+++ shop/views.py
@@ -55,8 +55,26 @@ def tracker(request):
 
     return render(request, 'shop/tracker.html')
 
+def searchMatch(query, item):
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower() or query in item.sub_category.lower():
+        return True
+    else:
+        return False
+
 def search(request):
-    return render(request, 'shop/search.html')
+    query = request.GET.get('search')
+    allprods = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category = cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+        n = len(prod)
+        nslides = n//4 + ceil((n/4) - (n//4))
+        if len(prod)!=0:
+            allprods.append([prod, range(1, nslides), nslides])
+    params = {'allprods': allprods}
+    return render(request, 'shop/index.html', params)
 
 def products(request, myid):
     product = Product.objects.filter(id = myid)
